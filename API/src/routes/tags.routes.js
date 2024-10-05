38e71f8d0dbf248a4c66b26f8bcbd2cefa4f579b
weslei <wesleivisentin@hotmail.com>
2024-10-05 17:37:48 -0300

    Server add

---
+++ API/src/routes/tags.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const TagsController = require('../controllers/TagsController')
+
+const tagsRoutes = Router()
+
+const tagsController = new TagsController()
+
+tagsRoutes.get('/', ensureAuthenticated, tagsController.index)
+
+module.exports = tagsRoutes
