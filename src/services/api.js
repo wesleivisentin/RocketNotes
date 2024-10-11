2a10c835f72389b98bacd6a0b60456ee36189ce5
weslei <wesleivisentin@hotmail.com>
2024-10-11 14:42:04 -0300

    add pnpm

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://rocketnotes-api-9rg7.onrender.com' // se não funcionar, botar localhost/
 })
